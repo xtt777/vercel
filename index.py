b1a8a4801ee491cd885eea765ef2376b634efda9
xtt777 <xxtong28@gmail.com>
2025-03-27 10:13:52 +0800

    v2_get

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def index():
     homepage = "<h1>郭芯彤Python網頁(時間+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=Amy&work=pu>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
     homepage += "<a href=/about>芯彤簡介網頁</a><br>"
     return homepage
@@ -30,7 +30,9 @@ def me():
 @app.route("/welcome", methods=["GET"])
 def welcome():
     user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
+    w = request.values.get("work")
+    return render_template("welcome.html", name=user,work = w)
+
 @app.route("/account", methods=["GET", "POST"])
 def account():
     if request.method == "POST":
